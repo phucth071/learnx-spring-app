2c0dd12713488357df84a3a3a30f0e9b47406a77
phucth071 <phucth0710@gmail.com>
2024-07-12 14:48:34 +0700

    Add header Authorization in swagger-ui

--- src/main/java/com/hcmute/utezbe/config/SwaggerConfig.java
+++ src/main/java/com/hcmute/utezbe/config/SwaggerConfig.java
@@ -8,11 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.Example;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -21,11 +28,24 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalRequestParameters(customRequestHeader())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfoMetaData());
+    }
+
+    private List<RequestParameter> customRequestHeader() {
+        RequestParameter requestParameter = new RequestParameterBuilder()
+                .name("Authorization")
+                .description("Bearer Token")
+                .in(ParameterType.HEADER)
+                .query(param -> param.model(model -> model.scalarModel(ScalarType.STRING)))
+                .required(false)
+//                .example(new Example("Bearer "))
+                .build();
+        return List.of(requestParameter);
     }
 
     private ApiInfo apiInfoMetaData() {
