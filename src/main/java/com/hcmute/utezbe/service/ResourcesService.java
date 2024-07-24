e4cea5a028867f1f85b825f6b9dbe3c6ef356d44
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2024-07-25 06:55:10 +0700

    CRUD Lecture + Resources

---
+++ src/main/java/com/hcmute/utezbe/service/ResourcesService.java
@@ -0,0 +1,44 @@
+package com.hcmute.utezbe.service;
+
+import com.hcmute.utezbe.auth.AuthService;
+import com.hcmute.utezbe.entity.Resources;
+import com.hcmute.utezbe.entity.enumClass.Role;
+import com.hcmute.utezbe.exception.AccessDeniedException;
+import com.hcmute.utezbe.repository.ResourcesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ResourcesService {
+
+    private final ResourcesRepository resourcesRepository;
+
+    public Optional<Resources> getResourcesById(Long id) {
+        return resourcesRepository.findById(id);
+    }
+
+    public List<Resources> getAllResources() {
+        return resourcesRepository.findAll();
+    }
+
+    public Resources saveResources(Resources resources) {
+        if (!AuthService.isUserHaveRole(Role.TEACHER) && !AuthService.isUserHaveRole(Role.ADMIN)) {
+            throw new AccessDeniedException("You do not have permission to do this action!");
+        }
+        return resourcesRepository.save(resources);
+    }
+
+    public Resources deleteResources(Long id) {
+        if (!AuthService.isUserHaveRole(Role.TEACHER) && !AuthService.isUserHaveRole(Role.ADMIN)) {
+            throw new AccessDeniedException("You do not have permission to do this action!");
+        }
+        Optional<Resources> resources = resourcesRepository.findById(id);
+        resources.ifPresent(resourcesRepository::delete);
+        return resources.orElse(null);
+    }
+
+}
